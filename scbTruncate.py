9d819a6e26a4b9d52ee25883c891cd4a1cf9bc33
unknown <Sean@.(none)>
2010-08-29 21:38:55 -0400

    SCBTruncate can select sub-set of agents
    scbTruncate has added some arguments which allow for a subset of agents
    as well as a subset of frames.  Currently, it simply takes the first N
    agents from the scene.  Ultimately, it would be nice if it could randomly
    select them.  That way, even if the definitions are spatially related,
    the sampling could be over the entire space.

--- scbTruncate.py
+++ scbTruncate.py
@@ -6,24 +6,28 @@ import sys
 
 DEFAULT_OUTPUT = 'output.scb'
 
-def copyNFrames( inFile, outFile, count, step ):
+def copyNFrames( inFile, outFile, count, step, tgtAgtCount ):
     """Copies at most count frames from the scb inFile to
     the outFile.  It copies every step-th frame.
     Reports number of frames copied.  Copying
     includes header"""
     # assume both inFile and outFile are at position 0
     # copy header - 4 bytes for version, 4 bytes for agent count
-    header = inFile.read( 8 )
+    header = inFile.read( 4 )
     outFile.write( header )
-    agtCount = struct.unpack( 'i', header[4:] )[0]
+    agtCount = struct.unpack( 'i', inFile.read( 4 ) )[0]
+    if ( tgtAgtCount == -1 or tgtAgtCount > agtCount):
+        tgtAgtCount = agtCount
+    outFile.write( struct.pack('i', tgtAgtCount ) )
     frameSize = agtCount * 4 * 3  # three, four-byte floats per agent
+    copySize = tgtAgtCount * 4 * 3  # three, four-byte floats per agent
     actual = 0
     readFrame = 0
     while ( actual < count ):
         data = inFile.read( frameSize )
         if ( data ):
             if ( readFrame % step == 0 ):
-                outFile.write( data )
+                outFile.write( data[:copySize] )
                 actual += 1
             readFrame += 1
         else:
@@ -34,7 +38,7 @@ def copyNFrames( inFile, outFile, count, step ):
 def usage():
     print "Truncate an scb file"
     print
-    print "scbTruncate -in filename <-out filename.scb> -n # -step #"
+    print "scbTruncate -in filename <-out filename.scb> -n # -step # <-agt *>"
     print
     print "  -in filename     - the name of the file to truncate"
     print "                     must be valid scb file"
@@ -44,6 +48,8 @@ def usage():
     print "                     if not provided, output file defaults to %s" % ( DEFAULT_OUTPUT )
     print "  -step #          - the number of frames between the sampled frames"
     print "                      defaults to 1 (i.e. EVERY frame"
+    print "  -agt #           - up to the first # agents will be exported"
+    print "                     by default, all are exported"
     sys.exit(1)
     
 def main():
@@ -52,7 +58,7 @@ def main():
     
     # parse command-line arguments
     try:
-        pMan = SimpleParamManager( sys.argv[1:], {'in':'', 'out':DEFAULT_OUTPUT, 'n':0, 'step':1 } )
+        pMan = SimpleParamManager( sys.argv[1:], {'in':'', 'out':DEFAULT_OUTPUT, 'n':0, 'step':1, 'agt':-1 } )
     except IOError:
         usage()
 
@@ -60,6 +66,7 @@ def main():
     outName = pMan[ 'out' ]
     count = int( pMan[ 'n' ] )
     step = int( pMan['step'] )
+    agentCount = int( pMan['agt'] )
     
     if ( not count ):
         print "ERROR! Invalid number of frames: ", count
@@ -75,7 +82,7 @@ def main():
         print "ERROR! Couldn't open output file:", outFile
         usage()
 
-    nCopied = copyNFrames( inFile, outFile, count, step )
+    nCopied = copyNFrames( inFile, outFile, count, step, agentCount )
     print "Copied %d frames into %s" % ( nCopied, outName )
 
 if __name__ == '__main__':
