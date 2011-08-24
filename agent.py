56a302631d1a622c1d5dd6c120d86eec97bb77d8
Sean Curtis <seanc@cs.unc.edu>
2011-08-24 10:50:06 -0400

    Factored agent and graph functionality into separate files
    The roadmapBuilder was overly full.  It contained definitions for graphs
    and agents rather than just focusing on view functionality.  I've moved
    the code around so this is no longer the case.

---
+++ agent.py
@@ -0,0 +1,260 @@
+from xml.sax import make_parser, handler
+from OpenGL.GL import *
+from math import cos, sin, pi
+import os
+
+class Agent:
+    """Agent object for drawing"""
+    ID = 0
+
+    INACTIVE = 0
+    AGENT = 1
+    GOAL = 2
+    def __init__( self, radius, pos, goal ):        
+        self.radius = radius
+        self.pos = pos
+        self.goal = goal
+        self.id = Agent.ID
+        self.active = Agent.INACTIVE
+        Agent.ID += 2
+
+    def __str__( self ):
+        return "Agent %d of %d - %s" % ( self.id, Agent.ID, self.pos )
+
+    def sjguy( self ):
+        return '%f %f %f %f' % ( self.pos[0], self.pos[1], self.goal[0], self.goal[1] )
+
+    def xml( self, defRadius ):
+        s = '\n\t<Agent p_x="{0}" p_y="{1}" '.format( self.pos[0], self.pos[1] )
+        if ( self.radius != defRadius ):
+            s += 'r="{0}" '.format( self.radius )
+        s += '/>'
+        return s
+    
+    def printActive( self ):
+        if ( self.active == Agent.GOAL ):
+            print "GOAL active"
+        elif ( self.active == Agent.AGENT ):
+            print "AGENT active"
+        else:
+            raise AttributeError, "Can't get active position for an inactive agent"
+        
+    def getActivePos( self ):
+        if ( self.active == Agent.GOAL ):
+            return self.goal
+        elif ( self.active == Agent.AGENT ):
+            return self.pos
+        else:
+            raise AttributeError, "Can't get active position for an inactive agent"
+        
+    def setActivePos( self, p ):
+        if ( self.active == Agent.GOAL ):
+            self.goal = p
+        elif ( self.active == Agent.AGENT ):
+            self.pos = p
+        else:
+            raise AttributeError, "Can't get active position for an inactive agent"
+        
+    def setPosition( self, pos ):
+        self.pos = pos
+
+    def setGoalPosition( self, pos ):
+        self.goal = pos
+
+    def activateAgent( self ):
+        self.active = Agent.AGENT
+
+    def activateGoal( self ):
+        self.active = Agent.GOAL        
+
+    def deactivate( self ):
+        self.active = False
+
+    def drawGL( self, select=False, editable=False ):
+        if ( self.active and not select ):
+            glColor3f( 0, 0, 1 )
+            glBegin( GL_LINES )
+            glVertex3f( self.pos[0], self.pos[1], 0 )
+            glVertex3f( self.goal[0], self.goal[1], 0 )
+            glEnd()
+
+        # draw the agent
+        if ( editable ):
+            glColor3f( 0, 0, 0.95 )
+        else:
+            glColor3f( 0, 0, 0.4 )
+
+        scale = self.radius
+        if ( self.active == Agent.AGENT ):
+            scale *= 1.5
+        glPushMatrix()            
+        glTranslate( self.pos[0], self.pos[1], 0 )
+        glScale( scale, scale, scale )
+        SEGMENTS = 10
+        dTheta = 2 * pi / SEGMENTS
+        if ( select ):
+            glLoadName( self.id )
+        glBegin( GL_TRIANGLE_FAN )
+        glVertex3f( 0, 0, 0 )
+        for i in range( SEGMENTS + 1 ):
+            theta = dTheta * i
+            c = cos( theta )
+            s = sin( theta )
+            glVertex3f( c, s, 0 )
+        glEnd()
+        glPopMatrix()
+        # draw the goal                    
+        if ( editable ):
+            glColor3f( 0.9, 0.45, 0.0 )
+        else:
+            glColor3f( 0.45, 0.225, 0 )
+
+        glPushMatrix()            
+        glTranslate( self.goal[0], self.goal[1], 0 )
+        scale = self.radius
+        if ( self.active == Agent.GOAL ):
+            scale *= 1.5
+        glScale( scale, scale, scale )
+        SEGMENTS = 10
+        if ( select ):
+            glLoadName( self.id + 1)
+        glBegin( GL_TRIANGLE_FAN )
+        glVertex3f( 0, 0, 0 )
+        for i in range( SEGMENTS + 1 ):
+            theta = dTheta * i
+            c = cos( theta )
+            s = sin( theta )
+            glVertex3f( c, s, 0 )
+        glEnd()
+        glPopMatrix()
+
+class AgentSet:
+    def __init__( self, defRadius ):
+        self.defRadius = defRadius
+        self.agents = []
+        self.activeAgent = None
+        self.editable = False
+
+    def setRadius( self, radius ):
+        '''Sets the default radius'''
+        self.defRadius = radius
+
+    def count( self ):
+        '''Returns the number of agents in the set'''
+        return len( self.agents )
+
+    def selectLastGoal( self ):
+        if ( self.activeAgent ):
+            self.activeAgent.deactivate()
+            self.activeAgent = None
+        if ( self.agents ):
+            self.activeAgent = self.agents[-1]
+            self.activeAgent.activateGoal()
+            
+    def selectAgent( self, id ):
+        """Given the goal or the Agent id, returns the agent with activity set correctly"""
+        agtId = id >> 1
+        agt = self.agents[ agtId ]
+        if ( id & 0x1 ):
+            agt.activateGoal()
+        else:
+            agt.activateAgent()
+        return agt
+
+    def initFromFile( self, file ):
+        print "Reading", file
+        base, ext = os.path.splitext( file )
+        if ( ext == '.txt' ):
+            f = open( file, 'r' )
+            aCount = int( f.readline() )
+            for line in f.xreadlines():
+                line = line.strip()
+                if ( line ):
+                    x1, y1, x2, y2 = map( lambda x: float(x), line.split() )
+                    self.agents.append( Agent( self.defRadius, (x1, y1), (x2,y2) ) )
+        else:
+            
+            parser = make_parser()
+            agtHandler = AgentXMLParser( self )
+            parser.setContentHandler( agtHandler )
+            parser.parse( file )
+
+    def sjguy( self ):
+        """Returns the stephen guy formatted agent set"""
+        s = '%d\n' % ( len( self.agents ) )
+        for a in self.agents:
+            s += '%s\n' % ( a.sjguy() )
+        return s
+
+    def xml( self, obstacles=None ):
+        '''Returns the xml-formatted agent set'''
+        #assumes everyone has the same goal
+        s = '''<?xml version="1.0"?>
+<Experiment time_step="0.05" >
+
+  <AgentSet obstacleSet="1" neighbor_dist="1.1" max_neighbors="10" r="{0}" class="0" g_r="3.0" pref_speed="1.04" ttc="0.5" max_speed="2.0" max_accel="2.0" >'''.format( self.defRadius )
+        for a in self.agents:
+            s += '  ' + a.xml( self.defRadius )
+        s += '\n  </AgentSet>'
+        if ( obstacles ):
+            s += obstacles.xml()
+        s += '\n</Experiment>'
+        return s
+    
+    def addAgent( self, pos, goal, radius = None ):
+        if ( radius == None ):
+            radius = self.defRadius
+        a = Agent( radius, pos, goal )
+        self.agents.append( a )
+        return a
+
+    def drawGL( self, select=False, dummy=None ):
+        glPushAttrib( GL_POLYGON_BIT )
+        glPolygonMode( GL_FRONT_AND_BACK, GL_FILL )
+        for agent in self.agents:
+            agent.drawGL( select, self.editable )
+        glPopAttrib()
+
+    def deleteActiveAgent( self ):
+        """Removes the active agent"""
+        if ( self.activeAgent ):
+            agt = self.activeAgent
+            activeID = agt.id
+            Agent.ID -= 2
+            i = activeID >> 1
+            popped = self.agents.pop( i )
+            assert( popped == agt )
+            for a in self.agents[ i:]:
+                a.id -= 2
+            self.activeAgent = None
+            return True
+        return False
+            
+
+class AgentXMLParser( handler.ContentHandler ):
+    def __init__( self, agentSet ):
+        self.agentSet = agentSet
+        self.goal = (0, 0)
+        self.readingGoal = False
+
+    def startElement( self, name, attrs ):
+        if ( name == 'Agent' ):
+            x = float( attrs[ 'p_x' ] )
+            y = float( attrs[ 'p_y' ] )
+            try:
+                r = float( attrs[ 'r' ] )
+            except:
+                r = self.agentSet.defRadius
+            if ( self.readingGoal ):
+                self.goal = ( x, y )
+            else:
+                self.agentSet.addAgent( (x, y), self.goal, r )
+        elif ( name == 'AgentSet' ):
+            self.agentSet.defRadius = float( attrs[ 'r' ] )
+        elif ( name == 'Goal' ):
+            self.readingGoal = True
+
+    def endElement( self, name ):
+        if ( name == 'Goal' ):
+            self.readingGoal = False
+            
---
+++ graph.py
@@ -0,0 +1,186 @@
+# All of the data structures for the road map (basically, a graph structure
+from OpenGL.GL import *
+
+class Vertex:
+    """Graph vertex"""
+    COUNT = 0
+    def __init__( self, pos ):
+        self.pos = pos
+        self.neighbors = []
+        self.id = Vertex.COUNT
+        Vertex.COUNT += 1
+
+    def __str__( self ):
+        return "%d %f %f" % ( len( self.neighbors ), self.pos[0], self.pos[1] )
+
+    def setPosition( self, pos ):
+        self.pos = ( pos[0], pos[1] )
+
+    def removeEdge( self, edge ):
+        i = self.neighbors.index( edge )
+        self.neighbors.pop( i )
+
+class Edge:
+    """Graph edge"""
+    def __init__( self, start=None, end=None ):
+        self.start = start
+        self.end = end
+
+    def __str__( self ):
+        return "%d to %d" % ( self.start.id, self.end.id )
+    
+    def isValid( self ):
+        return self.start != None and self.end != None
+
+    def clear( self ):
+        self.start = self.end = None
+
+    def copy( self ):
+        return Edge( self.start, self.end )
+    
+class Graph:
+    """Simple graph class"""
+    def __init__( self ):
+        self.vertices = []
+        self.edges = []
+        self.activeEdge = None
+        self.fromID = None
+        self.toID = None
+        self.testEdge = Edge()
+
+    def initFromFile( self, fileName ):
+        print "Roadmap initfromFile", fileName
+        f = open( fileName, 'r' )
+        vertCount = int( f.readline() )
+        for i in range( vertCount ):
+            line = f.readline()
+            tokens = line.strip().split()
+            x = float( tokens[2] )
+            y = float( tokens[3] )
+            self.addVertex( (x, y ) )
+        edgeCount = int( f.readline() )
+        for i in range( edgeCount ):
+            line = f.readline()
+            tokens = line.strip().split()
+            v1 = int( tokens[0] )
+            v2 = int( tokens[1] )
+            self.addEdgeByVert( v1, v2 )
+            
+    def __str__( self ):
+        s = "%d\n" % ( len (self.vertices ) )
+        for i, v in enumerate( self.vertices ):
+            s += '%d %s\n' % ( i, v )
+        s += '%d\n' % ( len( self.edges ) )
+        for i, e in enumerate( self.edges ):
+            p1 = e.start.pos
+            p2 = e.end.pos
+            dx = p1[0] - p2[0]
+            dy = p1[1] - p2[1]
+            dist = sqrt( dx * dx + dy * dy )
+            s += '%d %d %f\n' % ( e.start.id, e.end.id, dist )
+        return s
+
+    def lastVertex( self ):
+        """Returns the index of the last vertex"""
+        return self.vertices[-1]
+    
+    def addVertex( self, pos ):
+        self.vertices.append( Vertex( pos ) )
+
+    def deleteVertex( self, vertex ):
+        '''delete the given vertex and all edges attached to it'''
+        # first remove the vertex from the list
+        startIndex = self.vertices.index( vertex )
+        v = self.vertices.pop( startIndex )
+        # re-enumerate the following vertices
+        Vertex.COUNT -= 1
+        for i in range( startIndex, len( self.vertices ) ):
+            self.vertices[i].id = i
+
+        # remove edges            
+        for e in v.neighbors:
+            n = e.start
+            if ( n == vertex ):
+                n = e.end
+            n.removeEdge( e )
+            self.edges.pop( self.edges.index( e ) )
+
+        v.neighbors = []
+        
+    def addEdge( self, e ):
+        edge = e.copy()
+        edge.start.neighbors.append( edge )
+        edge.end.neighbors.append( edge )
+        self.edges.append( edge )
+    
+    def deleteEdge( self, edge ):
+        # delete the edge
+        edge.start.removeEdge( edge )
+        edge.end.removeEdge( edge )
+        self.edges.pop( self.edges.index( edge ) )
+        
+    def addEdgeByVert( self, v1, v2 ):
+        self.addEdge( Edge( self.vertices[ v1 ], self.vertices[ v2] ) )
+
+    def drawGL( self, select=False, selectEdges = False, editable=False ):
+        if ( selectEdges or not select ):
+            self.drawEdges( self.edges, select, editable )
+        glPointSize( 3.0 )
+        if ( not selectEdges ):
+            self.drawVertices( self.vertices, select, editable )
+        if ( self.fromID != None ):
+            glPointSize( 6.0 )
+            self.drawVertices( ( self.fromID, ), False, editable )
+        if ( self.toID != None ):
+            glPointSize( 6.0 )
+            self.drawVertices( ( self.toID, ), False, editable )
+        if ( self.activeEdge != None ):
+            glLineWidth( 3.0 )
+            self.drawEdges( ( self.activeEdge, ), False, editable )
+        
+    def drawEdges( self, edges, select=False, editable=False ):
+        if ( edges or self.testEdge.isValid() ):
+            glPushAttrib( GL_COLOR_BUFFER_BIT | GL_ENABLE_BIT )
+            glDisable( GL_DEPTH_TEST )
+            if ( editable ):
+                glColor3f( 0.0, 0.75, 0.0 )
+            else:
+                glColor3f( 0.0, 0.375, 0.0 )
+            for i, e in enumerate( edges ):
+                if ( select ):
+                    glLoadName( i )
+                glBegin( GL_LINES )
+                p1 = e.start.pos
+                p2 = e.end.pos
+                glVertex3f( p1[0], p1[1], 0 )
+                glVertex3f( p2[0], p2[1], 0 )
+                glEnd()
+            if ( self.testEdge.isValid() ):
+                glLineWidth( 3.0 )
+                glColor3f( 0.0, 1.0, 0.5 )
+                glBegin( GL_LINES )
+                p1 = self.testEdge.start.pos
+                p2 = self.testEdge.end.pos
+                glVertex3f( p1[0], p1[1], 0 )
+                glVertex3f( p2[0], p2[1], 0 )
+                glEnd()
+            glPopAttrib()
+            glLineWidth( 1.0 )            
+            
+    def drawVertices( self, vertices, select=False, editable=False ):
+        if ( vertices ):
+            glPushAttrib( GL_COLOR_BUFFER_BIT | GL_ENABLE_BIT )
+            glDisable( GL_DEPTH_TEST )
+            if ( editable ):
+                glColor3f( 0.9, 0.9, 0.0 )
+            else:
+                glColor3f( 0.45, 0.45, 0.0 )
+            
+            for i, v in enumerate( vertices ):
+                if ( select ):
+                    glLoadName( i )
+                glBegin( GL_POINTS )
+                p = v.pos
+                glVertex3f( p[0], p[1], 0 )
+                glEnd()
+            glPopAttrib()    
