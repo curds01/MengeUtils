0cb7f3fef33b345e5dd64138a9b3f042cea8597d
Sean Curtis <seanc@cs.unc.edu>
2012-02-22 22:15:10 -0500

    Updated crowd work for analyzeCrowd
    1. Fixed density calculation (bad distance function.)
    2. Added image sub-paths for density and speed -- appends to the output
       directory.
    3. Changed log messages slightly for better legibility.
    4. (This is the big one) Added flow analysis.

--- CrowdWork.py
+++ CrowdWork.py
@@ -2,11 +2,13 @@
 
 import time
 from PyQt4 import QtGui, QtCore
-from primitives import Vector2
-from scbData import FrameSet
+from primitives import Vector2, Segment, segmentsFromString
+from scbData import FrameSet, NPFrameSet
 from ColorMap import *
 import Crowd
 import os
+
+# TODO: Switch everything to NPFrameSet
 
 class CrowdAnalyzeThread( QtCore.QThread ):
     '''Class to perform the crowd analysis'''
@@ -32,53 +34,61 @@ class CrowdAnalyzeThread( QtCore.QThread ):
 
         R = self.data[ 'kernelSize' ]
 
-        def distFunc( dist, radiusSqd ):
+        def distFunc( X, Y, radiusSqd ):
             """Constant distance function"""
             # This is the local density function provided by Helbing
-            return 1.0 / ( pi * radiusSqd ) * exp( - (dist * dist / radiusSqd ) )        
+            dist = np.sqrt( X * X + Y * Y )
+            return 1.0 / ( np.pi * radiusSqd ) * np.exp( - (dist * dist / radiusSqd ) )        
 
-        dfunc = lambda x: distFunc( x, R * R )
+        dfunc = lambda x, y: distFunc( x, y, R * R )
 
         densityAction = self.data[ 'DENSE_ACTION' ]
         if ( densityAction == 1 or densityAction == 3 ):
             self.processMessage.emit( 'Computing densities...' )
             s = time.clock()
             grids.computeDensity( domainMin, domainSize, res, dfunc, 3 * R, frameSet )
-            self.processMessage.emit( 'done in %.2f seconds' % ( time.clock() - s ) )
+            self.processMessage.emit( '    done in %.2f seconds' % ( time.clock() - s ) )
         if ( densityAction >= 2 ):
-            imageName = os.path.join( self.data[ 'outDir' ], 'density_' )
+            imgPath = os.path.join( self.data[ 'outDir' ], 'density' )
+            if ( not os.path.exists( imgPath ) ):
+                os.makedirs( imgPath )
+            imageName = os.path.join( imgPath, 'density_' )
             self.processMessage.emit( 'Creating density images...' )
             s = time.clock()
             grids.makeImages( colorMap, imageName, 'density' )
             pygame.image.save( colorMap.lastMapBar(7), '%sbar.png' % ( imageName ) )
-            self.processMessage.emit( 'done in %.2f seconds' % ( time.clock() - s ) )
+            self.processMessage.emit( '    done in %.2f seconds' % ( time.clock() - s ) )
 
         speedAction = self.data[ 'SPEED_ACTION' ]
         if ( speedAction == 1 or speedAction == 3 ):
             self.processMessage.emit( 'Computing speeds...' )
             s = time.clock()
-            grids.computeSpeeds( domainMin, domainSize, res, 3 * R, frameSet, float( self.data[ 'timeStep' ] ), Crowd.GridFileSequence.BLIT_SPEED, int( self.data[ 'speedWindow' ] ) )
-            self.processMessage.emit( 'done in %.2f seconds' % ( time.clock() - s ) )
+            grids.computeSpeeds( domainMin, domainSize, res, 3 * R, frameSet, float( self.data[ 'timeStep' ] ), [], Crowd.GridFileSequence.BLIT_SPEED, int( self.data[ 'speedWindow' ] ) )
+            self.processMessage.emit( '    done in %.2f seconds' % ( time.clock() - s ) )
         if ( speedAction >= 2 ):
-            imageName = os.path.join( self.data[ 'outDir' ], 'speed_' )
+            imgPath = os.path.join( self.data[ 'outDir' ], 'speed' )
+            if ( not os.path.exists( imgPath ) ):
+                os.makedirs( imgPath )
+            imageName = os.path.join( imgPath, 'speed_' )
             self.processMessage.emit( 'Creating speed images...' )
             s = time.clock()
             grids.makeImages( colorMap, imageName, 'speed' )
             pygame.image.save( colorMap.lastMapBar(7), '%sbar.png' % ( imageName ) )
-            self.processMessage.emit( 'done in %.2f seconds' % ( time.clock() - s ) )            
+            self.processMessage.emit( '    done in %.2f seconds' % ( time.clock() - s ) )            
 
-##        advecAction = self.data[ 'ADVEC_ACTION' ]
-##        if ( advecAction == 1 or advecAction == 3 ):
-##            self.processMessage.emit( 'Computing advection...' )
-##            s = time.clock()
-##            grids.computeAdvecFlow( domainMin, domainSize, res, dfunc, 3.0, 3 * R, frameSet, self.data[ 'ADVEC_LINES' ] )
-##            self.processMessage.emit( 'done in %.2f seconds' % ( time.clock() - s ) )
-##        if ( advecAction >= 2 ):
-##            imageName = os.path.join( self.data[ 'outDir' ], 'advec_' )
-##            self.processMessage.emit( 'Creating flow advection images...' )
-##            s = time.clock()
-##            grids.makeImages( colorMap, imageName, 'advec' )
-##            pygame.image.save( colorMap.lastMapBar(7), '%sbar.png' % ( imageName ) )
-##            self.processMessage.emit( 'done in %.2f seconds' % ( time.clock() - s ) )        
+        frameSet = NPFrameSet( scbFile )
+        flowAction = self.data[ 'FLOW_ACTION' ]
+        if ( flowAction == 1 or flowAction == 3 ):
+            segments = segmentsFromString( self.data[ 'flowLines' ], Segment )
+            self.processMessage.emit( 'Computing flow across %d segments' % len(segments) )
+            s = time.clock()
+            Crowd.computeFlow( frameSet, segments, tempFile )
+            self.processMessage.emit( '    done in %.2f seconds' % ( time.clock() - s ) )
+        if ( flowAction >= 2 ):
+            self.processMessage.emit( 'Computing flow plots...' )
+            s=time.clock()
+            # plot the beasts
+            Crowd.plotFlow( tempFile, frameSet.simStepSize )
+            self.processMessage.emit( '    done in %.2f seconds' % ( time.clock() - s ) )        
         
         self.processMessage.emit( 'FINISHED' )
