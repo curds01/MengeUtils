89937418941547b8b970ab150d99d2289975bd76
Sean Curtis <seanc@cs.unc.edu>
2012-05-27 19:42:13 -0400

    fakeRotation smooths rotation
    It takes an optional window parameter so that the rotations get smoothed.
    Now, a large velocity limit can be applied so agents can turn quickly,
    but, at the same time, they aren't subject to every little twitch.

--- fakeRotation.py
+++ fakeRotation.py
@@ -12,26 +12,28 @@ DEF_VEL_LIMIT = 10.0 * np.pi / 180.0  # 10 degrees per sec
 # The minimum distance an agent has to travel in order to consider a possible angular change
 MOVE_THRESH = 0.001
 
-def addOrientation( data, maxThetaDelta ):
+def addOrientation( data, maxThetaDelta, window=1 ):
     '''Given an M x N x K array of simulation data,
     computes orientation of each agent such that between timesteps the agent's orientation cannot change
     by more than maxThetaDelta. The data is modified IN PLACE.
 
     @param data: an M x N x K numpy array.  Where there are M agents with N float attributes over K frames.
-    @param maxThetaDelta: a float.  The maximum allowable change in orientation for a single timestep
+    @param maxThetaDelta: a float.  The maximum allowable change in orientation for a single timestep.
+    @param window: an int.  The size of the window used to compute the finite differences.  I.e., the
+            angular change at frame i is the difference of the direction of velocity at i and i + window.
     '''
+    print maxThetaDelta
     TWO_PI = 2.0 * np.pi
     # compute the original orientation based on the first two time steps
-    k = 2
-    disp = data[ :, :2, k ] - data[ :, :2, 0 ]
+    disp = data[ :, :2, window ] - data[ :, :2, 0 ]
     dist = np.sqrt( np.sum( disp * disp, axis=1 ) )
     while ( np.sum( dist < MOVE_THRESH ) > 0 ):
-        k += 1
+        window += 1
         disp = data[ :, :2, k ] - data[ :, :2, 0 ]
         dist = np.sqrt( np.sum( disp * disp, axis=1 ) )
 
     # make sure I have meaningful displacements for EVERYONE
-    print "Initial direction computed from frame %d" % k 
+    print "Initial direction computed from frame %d" % window 
         
     angles = np.arctan2( disp[:,1], disp[:,0] )
     angles[ angles < 0 ] += TWO_PI
@@ -41,12 +43,12 @@ def addOrientation( data, maxThetaDelta ):
     #   1. compute the direction of travel from frame i to frame i + 1
     #   2. Compute delta = the angular difference between orientation at i and the orientation of the direction 
     #   3. Increment the orientation by max( maxThetaDelta, delta )
-    for f in xrange( 1, data.shape[2] - 1 ):
-        disp = data[ :, :2, f + 1 ] - data[ :, :2, f ]
+    for f in xrange( 1, data.shape[2] - window ):
+        disp = data[ :, :2, f + window ] - data[ :, :2, f ]
         dist = np.sqrt( np.sum( disp * disp, axis=1 ) )
         noMovement = dist < MOVE_THRESH
         angles = np.arctan2( disp[:,1], disp[:,0] )
-        angles[ angles < 0 ] += TWO_PI
+##        angles[ angles < 0 ] += TWO_PI
         angles[ noMovement ] = data[ noMovement, 2, f - 1 ]
 
         # detect the ones that have gone around the the periodic        
@@ -56,15 +58,20 @@ def addOrientation( data, maxThetaDelta ):
         periodic = absDelta > np.pi # all of the items where I've split over the period
         bigger = delta < 0  # the new angle is bigger than the prev angle
         smaller = delta > 0  # the new angle is smaller than the prev angle
-        angles[ bigger & periodic ] += TWO_PI
-        angles[ smaller & periodic ] -= TWO_PI
+        angles[ bigger & periodic ] -= TWO_PI
+        angles[ smaller & periodic ] += TWO_PI
+
+        delta = prevAngles - angles
+        absDelta = np.abs( delta )
+        periodic = absDelta > np.pi # all of the items where I've split over the period
+        assert( np.sum( periodic ) == 0 )
 
         # now clamp it to maximum velocity        
         delta = prevAngles - angles
         absDelta = np.abs( delta )
         clamp = absDelta > maxThetaDelta
         delta[ clamp ] *= maxThetaDelta / absDelta[ clamp ]
-        angles = prevAngles - delta
+        angles = prevAngles - ( delta / window )
 
         # now clamp it to the range [0, 2pi]
         tooSmall = angles < 0
@@ -79,14 +86,15 @@ def addOrientation( data, maxThetaDelta ):
         data[ :, 2, f ] = angles
 
     # final orientation is simply copied from second to last
-    data[ :, 2, -1 ] = data[ :, 2, -2 ]
+    for f in range( -window, 0 ):
+        data[ :, 2, f ] = data[ :, 2, f-1 ]
 
-def addSCBOrientation( inFile, outFile, maxVel ):
+def addSCBOrientation( inFile, outFile, maxVel, window=1 ):
     '''Given an scb file, adds orientation to it based on max angular velocity, saving the result to
     the outFile.'''
     scbData = NPFrameSet( inFile )
     fullData = scbData.fullData()
-    addOrientation( fullData, scbData.simStepSize * maxVel)
+    addOrientation( fullData, scbData.simStepSize * maxVel * window, window)
 
     scbData.close()
     writeNPSCB( outFile, fullData, scbData, scbData.version )  
@@ -101,6 +109,8 @@ def main():
                        action='store', dest='outFileName', default='' )
     parser.add_option( '-a', '--angularVelocity', help='The maximum angular velocity allowed (in radians/s).  Default is %g' % DEF_VEL_LIMIT,
                        action='store', dest='velocity', type='float', default=DEF_VEL_LIMIT )
+    parser.add_option( '-w', '--window', help='Number of frames overwhich to compute angular velocity.  This smooths the signal (default is 1)',
+                       action='store', dest='window', type='int', default=1 )
 
     options, args = parser.parse_args()
 
@@ -118,7 +128,7 @@ def main():
     print "\tOutput to:", outFile
     print "\tMaximum angular velocity: %g radians/s (%g deg/s)" % ( options.velocity, options.velocity / np.pi * 180.0 )
 
-    addSCBOrientation( options.inFileName, outFile, options.velocity )
+    addSCBOrientation( options.inFileName, outFile, options.velocity, options.window )
 
 
 if __name__ == '__main__':
