a00a6001a604d3d8147a4d8c6cd4404fad3446d9
unknown <ksuvee@gamma23.cs.unc.edu>
2012-06-21 10:14:20 -0400

    Seperate Kernel and Kernel2 into Kernels file

--- Crowd.py
+++ Crowd.py
@@ -23,6 +23,7 @@ import numpy as np
 import threading
 import time
 from ColorMap import *
+from Kernels import Kernel
 from qtcontext import GLLine
 from flow import *
 from primitives import Vector2
@@ -142,63 +143,63 @@ def threadOutput( outFile, buffer, bufferLock, startTime ):
             bufferLock.release()
             time.sleep( 1.0 )
 
-class Kernel:
-    """Distance function kernel"""
-    def __init__( self, radius, dFunc, cSize ):
-        """Creates a kernel to add into the grid.normalize
-        The kernel is a 2d grid (with odd # of cells in both directions.)
-        The cell count is determined by the radius and cell size.x
-        The cellsize is a tuple containing the width and height of a cell.
-        (it need not be square.)  The values in each cell are determined
-        by the distance of each cell from the center cell computed with
-        dFunc.  Each cell is given a logical size of cSize"""
-        hCount = int( 6 * radius / cSize.x )
-        if ( hCount % 2 == 0 ):
-            hCount += 1
-        o = np.arange( -(hCount/2), hCount/2 + 1) * cSize.x
-        X, Y = np.meshgrid( o, o )
-        self.data = dFunc( X, Y )
-
-
-    def max( self ):
-        return self.data.max()
-
-    def min( self ):
-        return self.data.min()
-
-    def sum( self ):
-        return self.data.sum()
-    
-    def __str__( self ):
-        return str( self.data )
-
-class Kernel2:
-    """Computes the contribution of an agent to it's surrounding neighborhood"""
-    # THE BIG DIFFERENCE between Kernel2 and Kernel:
-    #   Kernel assumes that every agent is centered on a cell, so the contribution
-    #   is a fixed contribution to the neighborhood
-    #   This kernel has fixed size, but the values change because it's computed based
-    #   on the actual world position of the agent w.r.t. the world position of the kernel center
-    #   So, this creates a generic kernel of the appropriate size, but then given a particular
-    #   center value and a particular position, computes the unique kernel (instance method)
-    def __init__( self, radius, cSize ):
-        # compute size: assume cSize is square
-        self.k = int( 6 * radius / cSize.x )
-        if ( self.k % 2 == 0 ):
-            self.k += 1
-        self.data = np.zeros( (self.k, self.k), dtype=np.float32 )
-        # world offsets from the center of the kernel
-        o = np.arange( -(self.k/2), self.k/2 + 1) * cSize.x
-        self.localX, self.localY = np.meshgrid( o, o )
-        
-    def instance( self, dfunc, center, position ):
-        '''Creates an instance of the sized kernel for this center and position'''
-        localPos = position - center
-        deltaX = ( self.localX - localPos.x ) ** 2
-        deltaY = ( self.localY - localPos.y ) ** 2
-        distSqd = deltaX + deltaY
-        # the distance function must take an array as an argument.
-        self.data = dfunc( distSqd )
+##class Kernel:
+##    """Distance function kernel"""
+##    def __init__( self, radius, dFunc, cSize ):
+##        """Creates a kernel to add into the grid.normalize
+##        The kernel is a 2d grid (with odd # of cells in both directions.)
+##        The cell count is determined by the radius and cell size.x
+##        The cellsize is a tuple containing the width and height of a cell.
+##        (it need not be square.)  The values in each cell are determined
+##        by the distance of each cell from the center cell computed with
+##        dFunc.  Each cell is given a logical size of cSize"""
+##        hCount = int( 6 * radius / cSize.x )
+##        if ( hCount % 2 == 0 ):
+##            hCount += 1
+##        o = np.arange( -(hCount/2), hCount/2 + 1) * cSize.x
+##        X, Y = np.meshgrid( o, o )
+##        self.data = dFunc( X, Y )
+##
+##
+##    def max( self ):
+##        return self.data.max()
+##
+##    def min( self ):
+##        return self.data.min()
+##
+##    def sum( self ):
+##        return self.data.sum()
+##    
+##    def __str__( self ):
+##        return str( self.data )
+##
+##class Kernel2:
+##    """Computes the contribution of an agent to it's surrounding neighborhood"""
+##    # THE BIG DIFFERENCE between Kernel2 and Kernel:
+##    #   Kernel assumes that every agent is centered on a cell, so the contribution
+##    #   is a fixed contribution to the neighborhood
+##    #   This kernel has fixed size, but the values change because it's computed based
+##    #   on the actual world position of the agent w.r.t. the world position of the kernel center
+##    #   So, this creates a generic kernel of the appropriate size, but then given a particular
+##    #   center value and a particular position, computes the unique kernel (instance method)
+##    def __init__( self, radius, cSize ):
+##        # compute size: assume cSize is square
+##        self.k = int( 6 * radius / cSize.x )
+##        if ( self.k % 2 == 0 ):
+##            self.k += 1
+##        self.data = np.zeros( (self.k, self.k), dtype=np.float32 )
+##        # world offsets from the center of the kernel
+##        o = np.arange( -(self.k/2), self.k/2 + 1) * cSize.x
+##        self.localX, self.localY = np.meshgrid( o, o )
+##        
+##    def instance( self, dfunc, center, position ):
+##        '''Creates an instance of the sized kernel for this center and position'''
+##        localPos = position - center
+##        deltaX = ( self.localX - localPos.x ) ** 2
+##        deltaY = ( self.localY - localPos.y ) ** 2
+##        distSqd = deltaX + deltaY
+##        # the distance function must take an array as an argument.
+##        self.data = dfunc( distSqd )
                 
 class AbstractGrid:
     '''A class to index into an abstract grid'''
@@ -830,7 +831,7 @@ class GridFileSequence:
         '''Creates a binary file representing the density scalar fields of each frame'''
         global ACTIVE_RASTER_THREADS
 
-        THREAD_COUNT = 7
+        THREAD_COUNT = 1
         # file output
         outFile = open( self.outFileName + '.density', 'wb' )
         outFile.write( struct.pack( 'ii', resolution[0], resolution[1] ) )  # size of grid
