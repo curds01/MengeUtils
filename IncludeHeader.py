34372c927d9cf4e515a3338d4968fcb23b8945e5
Sean Curtis <seanc@cs.unc.edu>
2012-08-20 19:51:12 -0400

    Added user-logon dependent path expansion

--- IncludeHeader.py
+++ IncludeHeader.py
@@ -1,7 +1,18 @@
 # This file contains standard including file for the objreader
 import os
 import sys
-TOP_DIR = os.path.dirname( os.path.dirname(__file__) )
+import getpass
 
-sys.path.insert( 0, TOP_DIR + r'\objreader' )
-sys.path.insert( 1, TOP_DIR + r'\density' )
+if ( getpass.getuser() == 'ksuvee' or getpass.getuser() == 'TofuYui' ):
+
+    TOP_DIR = os.path.dirname( os.path.dirname(__file__) )
+
+    sys.path.insert( 0, TOP_DIR + r'\objreader' )
+    sys.path.insert( 1, TOP_DIR + r'\julich' )
+    sys.path.insert( 2, TOP_DIR + r'\density' )
+
+elif ( getpass.getuser() == 'seanc' or getpass.getuser() == 'Sean'):
+    sys.path.insert( 0, r'\projects\crowd\fund_diag\paper\pre_density\experiment' )
+    sys.path.insert( 0, r'\projects\objreader' )
+    sys.path.insert( 0, r'\projects\seyfried\density' )
+
